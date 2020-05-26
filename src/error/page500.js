2a30bec6588be284686942323829889a6088bb40
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-05-26 09:09:43 +0300

    added page404,500 bounded to main-page.css

---
+++ src/error/page500.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../main_page/main_page.css';
+import './error.css';
+import page500 from './page500.jpg';
+
+export default class Page500 extends Component {
+    render() {
+        return(
+            <div>
+               <div className="Title">
+                   <h1>Error</h1>
+               </div>
+               <div className="Main-background">
+                  <img src={page500} className='error-image' alt="page not found"/>
+                  {/* <div>exception reason from server</div> */}
+                </div>
+            </div>
+        );
+    }
+}
