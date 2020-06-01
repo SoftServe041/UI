5fc93ddacf90222403ca278b96f6bfdc0586babe
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-06-01 15:57:14 +0300

    header, footer, ... with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,88 +1,84 @@
-
-import React, {Suspense, lazy, useState} from 'react';
-
+import React, { Suspense, lazy } from 'react';
 import Header from "./header/Header";
-import HeaderButtons from "./header/HeaderButtons"
-//import Footer from "./Footer/footer";
-import 'react-bootstrap';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import LoginMenu from "./header/LoginMenu";
+import Footer from "./Footer/footer";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+
+
 
 
 const Home = lazy(() => import('./main_page/Main_page'));
-//const RegPage = lazy(() => import('./registration/reg_page'));
-//const Billing = lazy(() => import('./Billing/billing'));
-//const NotFound = lazy(() => import('./error/page404'));
+const RegPage = lazy(() => import('./registration/reg_page'));
+const Page404 = lazy(() => import('./error/page404'));
 
 const LoadBody = () => (
     <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                {
-                    //<Route exact path="/registration" component={RegPage} />
-                    //<Route exact path="/add-card" component={Billing} />
-
-
-                    ////to be continued
-                    //<Route exact path="/about-our-company" component={Billing} />
-                    //<Route exact path="/account" component={Account} />
-                    //<Route default component={NotFound} />
-                }
-            </Switch>
-        </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/registration" component={RegPage} />
+          <Route default component={Page404} />
+        </Switch>
+      </Suspense>
     </Router>
 );
 
 
+
+
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.logIn = this.logIn.bind(this);
-        this.logOut = this.logOut.bind(this);
-        //this.setToken = this.setToken.bind(this);
-        this.state = {
-            ifLoggedIn: false,
-            token: '',
-            userId: 0
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+    this.state = {
+      ifLoggedIn: false,
+    };
+  }
 
-    logIn() {
-        this.setState({ifShowModal: true});
-    }
-
-
-    logOut() {
-        this.setState({ifShowModal: false});
-    }
-
-    /*
-    setToken(){
-        this.useState({token: () => {<LoginMenu />}})
-    }
-
-     */
+  logIn() {
+    this.setState({
+      ifShowModal: true
+    });
+  }
 
 
+  logOut() {
+    this.setState({
+      ifShowModal: false
+    });
 
-    render() {
-        return (
-            <div>
-
-                <Header/>
-                <HeaderButtons ifLoggedIn={this.state.ifLoggedIn} setToken={this.state.setToken}/>
-                <LoadBody/>
+  }
 
 
-                {
-                    //<Footer />
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Container fluid id='body'>
+        <div id='header'>
+          <Header/>
+        </div>
+        {/* <div id='frame-body' className='fluid'> */}
+          <div id='load-body'>
+            <LoadBody/> 
+          </div>
+        {/* </div> */}
+       
+        <div id='footer'>
+           <Footer/>
+        </div>
+        
+        
+        
+      </Container>
+      
+    );
+  }
 }
+
+
 
 
 export default App;
