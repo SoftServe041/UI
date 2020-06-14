36bd091191e53eb40652c7d7087696d752a499fd
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-06-14 10:42:15 +0300

    added admin component, admin.css

---
+++ src/admin/admin.js
@@ -0,0 +1,17 @@
+import React, { Component,} from 'react';
+import Users from './users.js';
+import { Button, Tab, Nav,} from "react-bootstrap";
+
+
+export default class Admin extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div><div/>
+        );
+    }
+}
+
