2a6b201ca74780a9eaec8b5d8509ba675a2680cc
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-06-19 00:10:48 +0300

    added new component hubs.js

---
+++ src/admin/hubs.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Hubs(props) {
+   
+    return (
+        <div className='component'>
+          
+        </div>
+    );
+}
+
+export default Hubs;
