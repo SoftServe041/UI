8cf33e3fc98e090543102195bcaa59652a9e3726
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-06-19 00:12:56 +0300

    added imports, admin folder with json file

--- src/admin/hubs.js
+++ src/admin/hubs.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-
+import { Table, Dropdown, DropdownButton, Button, Form, Modal, Row, Col } from "react-bootstrap";
+import cities from './admin_resource/cities.json';
+import axios from 'axios';
+import './admin.css';
 
 function Hubs(props) {
    
