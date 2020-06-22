0e3db96d54a02fe39da2c6544e9bca4b6c133073
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-06-22 10:22:34 +0300

    added transport.js component for admin

---
+++ src/admin/transport.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+
+function Transports(props) {
+    let url = 'http://localhost:9041/admin/transport';
+    let urlForTransportTypes = 'http://localhost:9041/admin/transport/types';
+  
+    return (
+        <div>
+          
+        </div>
+    );
+
+}
+
+
+export default Transports;
