e59f11e3e3ea0a16dc5c71ed66dd04530725b84e
mvp <dima.bobryshev.mvp@gmail.com>
2020-06-26 14:31:55 +0300

    add some changes

--- src/BillingDetails/BillingDetails.js
+++ src/BillingDetails/BillingDetails.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from "./Card/Card";
 import s from './BillingDetails.module.css'
+import {Modal,FormControl} from "react-bootstrap";
+
 
 const BillingDetails = (props) => {
     const data = props.data.data
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
             <div>
@@ -12,7 +19,49 @@ const BillingDetails = (props) => {
                 </p>
                 <div className={s.container}>
                     <div>
-                        <button className={s.button}>Add new card</button>
+                        <>
+                            <button className={s.button} onClick={handleShow}>
+                                Add new card
+                            </button>
+
+                            <Modal show={show} size="lg">
+                                <Modal.Header>
+                                    <Modal.Title>Creating new card</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <div>
+                                        <div className={s.modal_container}>
+                                            <span className={s.before_input_text}>Name of card:</span>
+                                            <FormControl className={s.input_style} type="text" placeholder="Enter name of credit card"/>
+                                        </div>
+                                        <div className={s.modal_container}>
+                                            <span className={s.before_input_text}>Card number:</span>
+                                            <FormControl className={s.input_style} type="text" placeholder="Enter numb of credit card"/>
+                                        </div>
+                                        <div className={s.modal_container}>
+                                            <span className={s.before_input_text}>CSC:</span>
+                                            <FormControl className={s.input_style} type="text" placeholder="Enter CSC of card"/>
+                                        </div>
+                                        <div className={s.modal_container}>
+                                            <span className={s.before_input_text}>Expiration date:</span>
+                                            <FormControl className={s.input_style} type="date" placeholder="Enter date of credit card"/>
+                                        </div>
+                                        <div className={s.modal_container}>
+                                            <span className={s.before_input_text}>Billing address:</span>
+                                            <FormControl className={s.input_style} type="text" placeholder="Enter billing address"/>
+                                        </div>
+                                    </div>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <button className={s.button} onClick={handleClose}>
+                                        Close
+                                    </button>
+                                    <button className={s.button} onClick={handleClose}>
+                                        Create
+                                    </button>
+                                </Modal.Footer>
+                            </Modal>
+                        </>
                     </div>
                 </div>
             </div>
